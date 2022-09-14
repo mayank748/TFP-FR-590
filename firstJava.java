82bafa6deeac54e1243a7ea5a7aac66c172b0f6a
mayank748 <56978989+mayank748@users.noreply.github.com>
2022-09-14 08:13:36 +0530

    Update firstJava.java

--- firstJava.java
+++ firstJava.java
@@ -2,5 +2,6 @@ class firstJava{
 	public static void main(String [] args){
 		System.out.println("hello world.... from master");
 		System.out.println("hello world.... from test demo");
+		System.out.println("udating directly in git hub");
 	}
 }
