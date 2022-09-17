c70669a9c94901f811b3a5b07906835c572e2cdb
Mayank <smayank748@gmail.com>
2022-09-17 09:11:20 +0530

    [Mayank]deleted the uwanted file and solution to get single number from random function

--- firstJava.java
+++
@@ -1,7 +0,0 @@
-class firstJava{
-	public static void main(String [] args){
-		System.out.println("hello world.... from master");
-		System.out.println("hello world.... from test demo");
-		System.out.println("test to check merge");
-	}
-}
